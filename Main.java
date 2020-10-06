cfe2349501783c1a3a2386cf65c01d8de0c9983f
zesha5816 <zesha5816@wrdsb.ca>
2020-10-06 13:42:17 +0000

    finished the example question

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 /**
  *
- * @author 
+ * @author Ahmad Zeshan
  */
 public class Main {
 
@@ -10,6 +10,13 @@ public class Main {
    */
   public static void main(String[] args) {
     // TODO code application logic here
-    
+    int count =1;
+		while(count <= 10){
+			count = count + 1;
+		}
+
+		for(int count2 = 1; count2 <= 10; count2 = count + 1){
+			System.out.println(count2)
+		}
   }
 }
